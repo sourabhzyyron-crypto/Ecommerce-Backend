f2e880efcf5d8236ee95fba03783ceb4ec2b3a3f
Shubham <sourabhzyyron@gmail.com>
2026-05-18 12:43:25 +0530

    Add JWT authentication support with user login functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [ProductsModule, PrismaModule, UsersModule],
+  imports: [
+    ProductsModule,
+    PrismaModule,
+    UsersModule,
+    JwtModule.register({
+      global: true, 
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' }, 
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,9 +4,13 @@ import { CreateUserDto } from './create-user.dto';
 import { LoginUserDto } from './login-user.dto';
 import * as bcrypt from 'bcrypt';
 import { ConflictException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async createUser(createUserDto: CreateUserDto) {
     const { name, email, password } = createUserDto;
@@ -34,6 +38,7 @@ export class UsersService {
     return result;
   }
   async loginUser(loginUserDto: LoginUserDto) {
+   
     const { email, password } = loginUserDto;
 
     const user = await this.prisma.user.findUnique({
@@ -49,8 +54,10 @@ export class UsersService {
     if (!isMatch) {
       throw new ConflictException('Invalid email or password');
     }
+    const payload = { sub: user.id, email: user.email, name: user.name };
+    const token = await this.jwtService.signAsync(payload);
 
     const { password: _, ...result } = user;
-    return { ...result, message: 'Login successful' };
+    return { ...result, token, message: 'Login successful' };
   }
 }
