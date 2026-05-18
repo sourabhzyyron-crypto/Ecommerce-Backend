825e4fef4b09883d8303ed8d0b19e9b7bc8686e6
Shubham <sourabhzyyron@gmail.com>
2026-05-18 11:06:20 +0530

    Added Register user API

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,13 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './create-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
-  createUser() {
-    return this.usersService.createUser();
-  }
-
-  @Get()
-  getUsers() {
-    return this.usersService.getUsers();
+  createUser(createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-
+import { CreateUserDto } from './create-user.dto';
+import * as bcrypt from 'bcrypt';
+import { ConflictException } from '@nestjs/common';
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
-  async createUser() {
-    return this.prisma.user.create({
+  async createUser(createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
+
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('User with this email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prisma.user.create({
       data: {
-        email: 'john@example.com',
-        name: 'John',
+        name,
+        email,
+        password: hashedPassword,
       },
     });
-  }
 
-  async getUsers() {
-    return this.prisma.user.findMany();
+    const { password: _, ...result } = user;
+    return result;
   }
 }
